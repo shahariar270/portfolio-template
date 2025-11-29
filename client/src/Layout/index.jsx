dc29b0002645516b2703e2f3837ceddde7f464b4
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 16:24:35 +0600

    path issue fixed

--- client/src/Layout/index.jsx
+++ client/src/Layout/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from './Sections/Header'
+import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
   return (
     <div className='st-portfolio--layout'>
       <Header />
+      <Outlet />
     </div>
   )
 }
--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -10,7 +10,7 @@ export const router = createBrowserRouter([
         children:[
             {
                 index:true,
-                path:<Home/>
+                element:<Home/>
             }
         ]
     }
