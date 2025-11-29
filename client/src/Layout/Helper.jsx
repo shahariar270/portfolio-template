22faa83e1e619b4283f5a5c0b60db88c7248d0f8
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 10:43:40 +0600

    Add header component and styles with menu integration

---
+++ client/src/Layout/Helper.jsx
@@ -0,0 +1,23 @@
+
+export const menuArray =[
+  {
+    label:'Home',
+    route:'/'
+  },
+  {
+    label:'About Me',
+    route:'/'
+  },
+  {
+    label:'Skill',
+    route:'/'
+  },
+  {
+    label:'Project',
+    route:'/'
+  },
+  {
+    label:'Contact',
+    route:'/'
+  },
+]
--- client/src/Layout/Sections/Header.jsx
+++ client/src/Layout/Sections/Header.jsx
@@ -1,10 +1,24 @@
+import { menuArray } from '@Layout/Helper'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <div className='st-portfolio--header'>
-      <div className="">SHAHARIAR</div>
-      <div className=""></div>
+      <div className="st-portfolio--logo">SHAHARIAR</div>
+      <div className="st-portfolio--menu">
+        {menuArray.map((item, index) => (
+          <Link key={index} >
+            <p>
+            {item.label}
+            </p>
+          </Link>
+        ))
+        }
+      </div>
+      <div className="st-portfolio--theme">
+        
+      </div>
     </div>
   )
 }
