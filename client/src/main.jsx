3ce37deff54d7a175395964edb46cd8f330b2620
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 08:37:29 +0600

    route setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
 import './assets/styles/main.scss'
+import { router } from './route';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <h1 style={{ color: "var(--color-primary)" }}>hdasjf</h1>
+    <RouterProvider router={router} />;
   </StrictMode>,
 )
