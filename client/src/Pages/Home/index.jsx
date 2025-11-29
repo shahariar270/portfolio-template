59742c1f41987be1b0ce155d588ad1650c1e24ab
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 21:11:00 +0600

    add new scss

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -3,8 +3,8 @@ import profile from '../../assets/images/profile.jpg'
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="">
+    <div className="st-portfolio--home">
+      <div className="st-portfolio--home__content">
         <h1>Hello i This Shahariar</h1>
         <p>I build dynamic web applications and interactive experiences using modern web technologies</p>
       </div>
