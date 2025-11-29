ad78464383fced0d31a1623c0b7c4e571d3dda9a
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 09:47:26 +0600

    vite issue fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@Layout': path.resolve(__dirname, './src/Layout'),
+      '@Layout': path.resolve(__dirname, './src/Layout')
     }
   }
 })
