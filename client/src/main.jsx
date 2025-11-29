6f2bd8567fc722186694420848ba4b9948fd2e5a
shahariar270 <mdshahariar102@gmail.com>
2025-11-29 08:01:47 +0600

    color patele setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './assets/styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>hdasjf</h1>
+   <h1 style={{ color: "var(--color-primary)" }}>hdasjf</h1>
   </StrictMode>,
 )
