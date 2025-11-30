b99e5868483e29b83c751acaa60b0b931a1f8084
shahariar270 <mdshahariar102@gmail.com>
2025-11-30 21:33:00 +0600

    re design layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,6 @@ import { router } from './route';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />;
+    <RouterProvider router={router} />
   </StrictMode>,
 )
