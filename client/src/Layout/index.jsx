f1c069952ac37933881d620cb5a2a66045531cce
shahariar270 <mdshahariar102@gmail.com>
2025-12-01 21:25:39 +0600

    right compo added

--- client/src/Layout/index.jsx
+++ client/src/Layout/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { LeftContentBox } from './Sections/LeftContentBox'
 import { Outlet } from 'react-router-dom'
+import { RightContentBox } from './Sections/RightContentBox'
 
 export const Layout = () => {
   return (
     <div className='st-portfolio--layout'>
       <LeftContentBox />
-      <Outlet />
+      <RightContentBox />
     </div>
   )
 }
