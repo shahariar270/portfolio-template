2f261d607bdbb15fbccb9bc723e320f598141878
shahariar270 <mdshahariar102@gmail.com>
2025-12-01 22:21:11 +0600

    project setup

--- client/src/route/index.jsx
+++ client/src/route/index.jsx
@@ -2,6 +2,7 @@ import { Layout } from "@Layout/index";
 import { RightContentBox } from "@Layout/Sections/RightContentBox";
 import { About } from "@Pages/About";
 import Home from "@Pages/Home";
+import { Project } from "@Pages/Project";
 import { Skill } from "@Pages/Skill";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
             {
                 path:'/skill',
                 element:<Skill/>
+            },
+            {
+                path:'/project',
+                element:<Project/>
             },
         ]
     }
