e5e687ec3eab7958bc2507d87f1cb76bf9dc7eca
shahariar270 <mdshahariar102@gmail.com>
2025-12-05 21:39:15 +0600

    cannect skill page

--- client/src/Pages/Skill/index.jsx
+++ client/src/Pages/Skill/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { skillCards } from "./helper";
+import { SkillCard } from "./SkillCard";
+
 
 export const Skill = () => {
   return (
-    <div>Skill</div>
-  )
-}
+    <div className="skills-wrapper">
+      {skillCards.map((card, i) => (
+        <SkillCard
+          key={i} 
+          front={card.front} 
+          icon={card.icon} 
+          back={card.back} 
+        />
+      ))}
+    </div>
+  );
+};
