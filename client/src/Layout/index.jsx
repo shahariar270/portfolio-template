d100bfe7a6dafcb6442c3953d3dfdee33e17e048
shahariar270 <mdshahariar102@gmail.com>
2025-12-05 21:07:26 +0600

    add mouse move animation

--- client/src/Layout/index.jsx
+++ client/src/Layout/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LeftContentBox } from './Sections/LeftContentBox'
 import { RightContentBox } from './Sections/RightContentBox'
 
 export const Layout = () => {
+
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+
+    const handleMove = (e) => {
+      if (!cursor) return;
+      cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    };
+
+    document.addEventListener("mousemove", handleMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMove);
+    };
+  }, []);
+
   return (
     <div className='st-portfolio--layout'>
+      <div className="cursor"></div>
+
       <LeftContentBox />
       <RightContentBox />
     </div>
