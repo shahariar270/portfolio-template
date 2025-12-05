22142a8a24a4808044fbc11769097be30cd9baf5
shahariar270 <mdshahariar102@gmail.com>
2025-12-05 21:37:24 +0600

    update skillcard componenet

--- client/src/Pages/Skill/SkillCard.jsx
+++ client/src/Pages/Skill/SkillCard.jsx
@@ -1,19 +1,22 @@
-export const SkillCard = ({ front, back }) => {
-  return (
-    <div className="flip-card">
-      <div className="flip-inner">
+export const SkillCard = ({ front, icon, back }) => {
+    return (
+        <div className="flip-card">
+            <div className="flip-inner">
 
-        <div className="flip-front">
-          {front}
+                {/* FRONT */}
+                <div className="flip-front">
+                    <i className={icon}></i>
+                    <h3>{front}</h3>
+                </div>
+
+                {/* BACK */}
+                <div className="flip-back">
+                    {back.map((item, i) => (
+                        <p key={i}>{item}</p>
+                    ))}
+                </div>
+
+            </div>
         </div>
-
-        <div className="flip-back">
-          {back.map((item, i) => (
-            <p key={i}>{item}</p>
-          ))}
-        </div>
-
-      </div>
-    </div>
-  );
+    );
 };
