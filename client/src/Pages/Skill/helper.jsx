272186073c6b93e1d3e98791da52e5f50049753e
shahariar270 <mdshahariar102@gmail.com>
2025-12-05 21:36:54 +0600

    added skill helper

---
+++ client/src/Pages/Skill/helper.jsx
@@ -0,0 +1,112 @@
+export const skillCards = [
+    {
+        front: "HTML",
+        icon: "st-icon--html",
+        back: [
+            "Semantic Markup",
+            "SEO Friendly Structure",
+            "Forms & Validation",
+            "Best Practices"
+        ]
+    },
+    {
+        front: "CSS",
+        icon: "st-icon--css",
+        back: [
+            "Flexbox & Grid",
+            "Animations",
+            "Responsive Design",
+            "Sass / Tailwind"
+        ]
+    },
+    {
+        front: "JavaScript",
+        icon: "st-icon--javascript",
+        back: [
+            "ES6+ Features",
+            "Async/Await",
+            "DOM & Events",
+            "API Handling"
+        ]
+    },
+    {
+        front: "React",
+        icon: "st-icon--react",
+        back: [
+            "Hooks",
+            "State Management",
+            "Routing",
+            "UI Optimization"
+        ]
+    },
+    {
+        front: "Redux",
+        icon: "st-icon--redux",
+        back: [
+            "Store & Reducers",
+            "Actions",
+            "Manual Setup",
+            "Redux Toolkit"
+        ]
+    },
+    {
+        front: "Node.js",
+        icon: "st-icon--node",
+        back: [
+            "REST APIs",
+            "Express.js",
+            "Middleware",
+            "Authentication"
+        ]
+    },
+    {
+        front: "MongoDB",
+        icon: "st-icon--mongodb",
+        back: [
+            "Mongoose",
+            "Data Modeling",
+            "Aggregation",
+            "CRUD Operations"
+        ]
+    },
+    {
+        front: "Git",
+        icon: "st-icon--git",
+        back: [
+            "Version Control",
+            "Branching",
+            "Merging",
+            "Best Practices"
+        ]
+    },
+    {
+        front: "GitHub",
+        icon: "st-icon--github",
+        back: [
+            "Repositories",
+            "Pull Requests",
+            "Actions",
+            "Collaboration"
+        ]
+    },
+    {
+        front: "VS Code",
+        icon: "st-icon--vscode",
+        back: [
+            "Extensions",
+            "Debugging",
+            "Shortcuts",
+            "Workspace Management"
+        ]
+    },
+    {
+        front: "WordPress",
+        icon: "st-icon--wordpress",
+        back: [
+            "Theme Customization",
+            "Plugins",
+            "Elementor / Gutenberg",
+            "Website Optimization"
+        ]
+    }
+];
