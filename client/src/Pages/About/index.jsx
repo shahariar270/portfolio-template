99ad37d85d412ef3f3f1002788b1b4ebaa8f0c98
shahariar270 <mdshahariar102@gmail.com>
2025-12-05 20:18:46 +0600

    improve style

--- client/src/Pages/About/index.jsx
+++ client/src/Pages/About/index.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export const About = () => {
     return (
         <div className='st-portfolio--about'>
-            <h2>Who is am   </h2>
+            <h2>Who is am ?  </h2>
+            <div className='st-portfolio--about__paragraph'>
+                <span>
+                    I’m a MERN stack developer who enjoys building clean,
+                    functional, and user-friendly web apps. I work mainly
+                    with React, Redux, Node.js, and MongoDB. I like solving
+                    problems, improving UI/UX, and writing code that’s simple,
+                    clear, and scalable. I’m always learning new techniques and
+                    improving my workflow to build better digital experiences.
+                </span>
+            </div>
+
         </div>
     )
 }
