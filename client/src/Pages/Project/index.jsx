d8ae3ff754876389d232bb4d38affe30cf2c77b6
shahariar270 <mdshahariar102@gmail.com>
2025-12-06 09:34:21 +0600

    set path

--- client/src/Pages/Project/index.jsx
+++ client/src/Pages/Project/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { menuArray } from "./helper";
 
 export const Project = () => {
+  const [selectedTab, setSelectedTab] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const tab = params.get("tab");
+    if (tab) setSelectedTab(tab);
+  }, []);
+
+  const handleTabClick = (label) => {
+    setSelectedTab(label);
+
+    const params = new URLSearchParams(window.location.search);
+    params.set("tab", label);
+
+    window.history.replaceState(
+      {},
+      "",
+      `${window.location.pathname}?${params.toString()}`
+    );
+
+
+  };
+
   return (
-    <div>Project</div>
-  )
-}
+    <div className="st-portfolio--project">
+      <ul className="project-menu">
+        {menuArray.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => handleTabClick(item.value)}
+            className={selectedTab === item.value ? "active-tab" : ""}
+          >
+            {item.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
