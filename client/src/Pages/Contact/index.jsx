34e7f41c61407748cef150f81258aea079c82eb8
shahariar270 <mdshahariar102@gmail.com>
2025-12-06 20:31:42 +0600

    formik setups

--- client/src/Pages/Contact/index.jsx
+++ client/src/Pages/Contact/index.jsx
@@ -1,7 +1,43 @@
+import Button from '@Component/Button'
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
 
 export const Contact = () => {
+  const handleSubmit = ({ values }) => {
+
+  }
+
   return (
-    <div>Contect</div>
+    <Formik
+      initialValues={
+        {
+          name: '',
+          email: '',
+          content: ''
+        }}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <Field
+          as='input'
+          name='name'
+          placeholder='Enter your Name'
+        />
+        <Field
+          as='input'
+          name='email'
+          placeholder='Enter your Name'
+        />
+        <Field
+          as='textarea'
+          name='content'
+          placeholder='Enter your Name'
+        />
+        <Button
+          type='submit'
+          label={'Submit Value'}
+        />
+      </Form>
+    </Formik>
   )
 }
