ebbf0f41c9c27bf2b0a9317d800f1ea4d27dac1a
shahariar270 <mdshahariar102@gmail.com>
2025-12-07 05:56:12 +0600

    node mailer supported

--- client/src/Pages/Contact/index.jsx
+++ client/src/Pages/Contact/index.jsx
@@ -3,8 +3,26 @@ import { Field, Form, Formik } from 'formik'
 import React from 'react'
 
 export const Contact = () => {
-  const handleSubmit = ({ values }) => {
+  const handleSubmit = async ( values , {resetForm}) => {
+    try {
+      const response = await fetch("http://localhost:3000/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      })
+      const data = await response.json();
 
+      if (data.success) {
+        alert("Message sent successfully!");
+      } else {
+        alert("Failed to send message.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Error sending message.");
+    } finally {
+      resetForm()
+    }
   }
 
   return (
--- server/app.js
+++ server/app.js
@@ -1,20 +1,56 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const dotenv =require('dotenv');
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT ?? 3000;
 
 dotenv.config();
 app.use(express.json());
 app.use(cors())
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'shahariardorjoy52@gmail.com',
+        pass: 'spbv blin hbfu giit'
+    }
+})
+
+app.post("/contact", async (req, res) => {
+    const { name, email, content } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: "shahariardorjoy52@gmail.com", 
+        subject: "New Contact Form Message",
+        text: `
+নতুন একটি মেসেজ পেয়েছেন!
+
+Name: ${name}
+Email: ${email}
+Message: ${content}
+    `,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.json({ success: true, message: "Email sent!" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Email failed" });
+    }
+});
+
+
+
 //home route
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('request send successfully')
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is ruing port ${PORT}`);
 })
 
